1ae0198996d9e8076ab152e9d478c7dd913aa8dd
proller <proller@github.com>
2013-07-14 03:45:50 +0400

    du

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cd ..
 git clone https://github.com/proller/home.git
 git up home
 #ln -s home/* home/.* ./
-ln -s home/.* ./
+ln -vs home/.* ./
 #rm install.sh
 chmod go-rw .ssh/* .login_conf
 chmod go-rwx .ssh/id_rsa.git-credential-cache
